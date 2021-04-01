8f0abd694790298c8d841a2908570d89fba0a5cd
Rajeev Gupta <rrpit9@gmail.com>
2021-04-01 17:43:29 +0530

    Environment setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  projectPath : '/dev/winz-front',
+  projectPath : '/',
   stripeKey : 'pk_test_TYooMQauvdEDq54NiTphI7jx',
   stripeSecret : 'sk_test_4eC39HqLyjWDarjtT1zdp7dc',
   apiUrl : 'http://127.0.0.1:8000/api/v1/',
