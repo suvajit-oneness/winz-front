4e3c0effcccd0e96067e44a032a0e1d9c9a116cf
Rajeev Gupta <rrpit9@gmail.com>
2021-04-14 16:54:12 +0530

    Policies

--- src/app/component/faq/faq.component.ts
+++ src/app/component/faq/faq.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { APIService } from 'src/app/service/api.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { exit } from 'process';
 
 @Component({
   selector: 'app-faq',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  public setting: {data: SETTINGDATA[];};
 
-  ngOnInit(): void {
+  constructor(private _api:APIService,private _loader : NgxUiLoaderService) {
+    this.setting = {data : []}; // Initialise blank data to SCHEDULE interface
   }
 
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+    this.settingData();
+  }
+
+  public settingData(){
+    this._loader.startLoader('loader');
+    this._api.getSettingSEOData().subscribe(
+      res => {
+        this.setting.data = res.data;
+        this._loader.stopLoader('loader');
+      },
+      err => {
+        this._loader.stopLoader('loader');
+      }
+    )
+  }
+
+  getValueUsingKey(key){
+    let response = '';
+    this.setting.data.forEach((value) => {
+      if(value.key === key){
+        response = value.value
+        exit;
+      }
+    });
+    // console.log('getting value here',response);
+    return response;
+  }
+
+}
+
+interface SETTINGDATA{
+  id : number,
+  key : string,
+  value : string,
+  created_at : string,
+  updated_at : string,
+  deleted_at : string,
 }
