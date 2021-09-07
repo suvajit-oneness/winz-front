6bc69056c07589f853399a23a3f8e01ccdb6b544
Rajeev <rrpit9@gmail.com>
2021-09-07 12:32:42 +0530

    Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: true,
-  projectPath : '/dev/winz-front',
-  whatsappNumber : '918804809613',
+  projectPath : '/',
+  whatsappNumber : '',
   stripeKey : 'pk_test_TYooMQauvdEDq54NiTphI7jx',
   stripeSecret : 'sk_test_4eC39HqLyjWDarjtT1zdp7dc',
   // apiUrl : 'http://127.0.0.1:8000/'
-  apiUrl : 'https://demo91.co.in/dev/winz-v2/public/'
+  apiUrl : 'https://emarketernetwork.com/winz-backend/public/'
+  // apiUrl : 'https://demo91.co.in/dev/winz-v2/public/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,12 @@
 export const environment = {
   production: false,
   projectPath : '/',
-  whatsappNumber : '918804809613',
+  whatsappNumber : '',
   stripeKey : 'pk_test_TYooMQauvdEDq54NiTphI7jx',
   stripeSecret : 'sk_test_4eC39HqLyjWDarjtT1zdp7dc',
   // apiUrl : 'http://127.0.0.1:8000/'
-  apiUrl : 'https://demo91.co.in/dev/winz-v2/public/',
+  apiUrl : 'https://emarketernetwork.com/winz-backend/public/'
+  // apiUrl : 'https://demo91.co.in/dev/winz-v2/public/'
 };
 
 /*
