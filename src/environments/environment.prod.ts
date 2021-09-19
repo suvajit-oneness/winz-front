04dea0abf2a72afc1e177b7de30e959e30d01070
Rajeev <rrpit9@gmail.com>
2021-09-19 21:05:07 +0530

    ddd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   stripeKey : 'pk_test_TYooMQauvdEDq54NiTphI7jx',
   stripeSecret : 'sk_test_4eC39HqLyjWDarjtT1zdp7dc',
   // apiUrl : 'http://127.0.0.1:8000/',
-  apiUrl : 'http://admin.winzinternational.com/',
+  apiUrl : 'https://admin.winzinternational.com/',
   // apiUrl : 'https://demo91.co.in/dev/winz-v2/public/',
 };
