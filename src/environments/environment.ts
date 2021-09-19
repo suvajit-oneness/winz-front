1dc45bc6ce5f8da3b12a3ec0ad261bef2a3beddb
Rajeev <rrpit9@gmail.com>
2021-09-19 20:46:44 +0530

    Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,9 +8,9 @@ export const environment = {
   whatsappNumber : '',
   stripeKey : 'pk_test_TYooMQauvdEDq54NiTphI7jx',
   stripeSecret : 'sk_test_4eC39HqLyjWDarjtT1zdp7dc',
-  // apiUrl : 'http://127.0.0.1:8000/'
-  apiUrl : 'http://backend.winzinternational.com/'
-  // apiUrl : 'https://demo91.co.in/dev/winz-v2/public/'
+  // apiUrl : 'http://127.0.0.1:8000/',
+  apiUrl : 'http://admin.winzinternational.com/',
+  // apiUrl : 'https://demo91.co.in/dev/winz-v2/public/',
 };
 
 /*
