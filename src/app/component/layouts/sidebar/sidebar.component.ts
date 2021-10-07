a677662e4d10b0906ccdad9a7e9ef754e74b15c2
Jarvicx <chakraborty.arindam27@gmail.com>
2021-10-07 22:55:51 +0530

    change-dashboard

--- src/app/component/layouts/sidebar/sidebar.component.ts
+++ src/app/component/layouts/sidebar/sidebar.component.ts
@@ -8,15 +8,15 @@ import { APIService } from 'src/app/service/api.service';
       <li routerLinkActive="active"><a routerLink="/user/profile"><i class="ti-pencil-alt"></i> Edit Profile</a></li>
       <li routerLinkActive="active"><a routerLink="/user/password/change"><i class="ti-eraser"></i> Change Password</a></li>
       <!-- <li routerLinkActive="active"><a routerLink="/user/subscribed/course"><i class="ti-receipt"></i> Subscribed Courses</a></li> -->
-      <li routerLinkActive="active" *ngIf="teacherProfile"><a routerLink="/teacher/course/list"><i class="ti-book"></i> Your Courses</a></li>
+      <li routerLinkActive="active" *ngIf="teacherProfile"><a routerLink="/teacher/course/list"><i class="ti-agenda"></i> Your Courses</a></li>
       <!-- <li routerLinkActive="active" *ngIf="teacherProfile"><a routerLink="/user/chapter"><i class="ti-book"></i> Chapters</a></li> -->
-      <li routerLinkActive="active" *ngIf="teacherProfile"><a routerLink="/booking-request"><i class="ti-book"></i> Booking Request</a></li>
+      <li routerLinkActive="active" *ngIf="teacherProfile"><a routerLink="/booking-request"><i class="ti-bookmark-alt"></i> Booking Request</a></li>
       <li routerLinkActive="active" *ngIf="userProfile"><a routerLink="/slot-booking-history"><i class="ti-book"></i> Slot Booking History</a></li>
-      <li routerLinkActive="active" *ngIf="teacherProfile"><a routerLink="/schedule"><i class="ti-book"></i> Schedule</a></li>
-      <li routerLinkActive="active" *ngIf="teacherProfile"><a routerLink="/events"><i class="ti-book"></i> Events</a></li>
+      <li routerLinkActive="active" *ngIf="teacherProfile"><a routerLink="/schedule"><i class="ti-calendar"></i> Schedule</a></li>
+      <li routerLinkActive="active" *ngIf="teacherProfile"><a routerLink="/events"><i class="ti-cup"></i> Events</a></li>
       <li routerLinkActive="active"><a routerLink="/chaper/purchase/history"><i class="ti-book"></i> Subscribed Chapter</a></li>
-      <li routerLinkActive="active"><a routerLink="/user/membership"><i class="ti-book"></i> Your Membership</a></li>
-      <li routerLinkActive="active"><a routerLink="/user/zoom-meeting"><i class="ti-book"></i> Zoom Meetings</a></li>
+      <li routerLinkActive="active"><a routerLink="/user/membership"><i class="ti-id-badge"></i> Your Membership</a></li>
+      <li routerLinkActive="active"><a routerLink="/user/zoom-meeting"><i class="ti-video-camera"></i> Zoom Meetings</a></li>
       <li><a href="javascript:void(0)" (click)="userLogout()"><i class="ti-na"></i> Log Out</a></li>
   `,
   styles: [
