de6cb2e8517ef5ad8a7ce984eabd4b463f225ca2
Rajeev <rrpit9@gmail.com>
2021-10-08 10:20:31 +0530

    Whatsapp umer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   projectPath : '/',
-  whatsappNumber : '',
+  whatsappNumber : '+85265368907',
   stripeKey : 'pk_test_TYooMQauvdEDq54NiTphI7jx',
   stripeSecret : 'sk_test_4eC39HqLyjWDarjtT1zdp7dc',
   // apiUrl : 'http://127.0.0.1:8000/',
